d21343dc9848a3d26df0169a89726deda0800523
Guilherme Gama <guilhermegamabs@gmail.com>
2024-02-18 12:37:23 -0300

    Update index.ts

--- src/A0017-type-assertions/index.ts
+++ src/A0017-type-assertions/index.ts
@@ -1,1 +1,14 @@
-const body = document.querySelector('body');
+const body1 = document.querySelector('body');
+if (body1) body1.style.background = 'red';
+
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+const input = document.querySelector('.input') as HTMLInputElement;
+input.value = 'Qualquer coisa';
+input.focus();
+
+const body4 = document.querySelector('body') as unknown as number;
