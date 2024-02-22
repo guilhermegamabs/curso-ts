6965fa3331a34d6773e3b6085a162fabbd71d43f
Guilherme Gama <guilhermegamabs@gmail.com>
2024-02-22 19:38:50 -0300

    getters-setters

---
+++ src/A0026-getters-setters/index.ts
@@ -0,0 +1,20 @@
+export class Pessoa {
+  constructor(
+    private nome: string,
+    private sobrenome: string,
+    private idade: number,
+    private _cpf: string,
+  ) {}
+
+  set cpf(cpf: string) {
+    this._cpf = cpf;
+  }
+
+  get cpf(): string {
+    return this._cpf.replace(/\D/g, '');
+  }
+}
+
+const pessoa = new Pessoa('Guilherme', 'Gama', 19, '111.111.111-11');
+pessoa.cpf = '123.456-789-10';
+console.log(pessoa);
