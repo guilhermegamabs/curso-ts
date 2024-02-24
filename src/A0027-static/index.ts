ae3ba0f52704a4f1d16b65b560e12daa204d0f6a
Guilherme Gama <guilhermegamabs@gmail.com>
2024-02-24 16:24:34 -0300

    Static

---
+++ src/A0027-static/index.ts
@@ -0,0 +1,18 @@
+export class Pessoa {
+  static idadePadrao = 0;
+  static cpfPadrao = '000.000.000-00';
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public idade: number,
+    public cpf: string,
+  ) {}
+
+  static criaPessoa(nome: string, sobrenome: string): Pessoa {
+    return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+}
+
+const pessoa = new Pessoa('Guilherme', 'Gama', 19, '111.111.111-11');
+const pessoa2 = Pessoa.criaPessoa('João', 'Alberto');
+console.log(pessoa2);
